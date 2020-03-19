192a40eb1b412f32948fea16285f3bc97bf93d1c
MoulaYounes <MoulaYounes@DESKTOP-GBNILO9>
2020-03-19 10:26:02 +0100

    generation crud

--- src/main/java/com/zsmart/ir/ws/rest/converter/IRConverter.java
+++ src/main/java/com/zsmart/ir/ws/rest/converter/IRConverter.java
@@ -1,264 +1,265 @@
 package com.zsmart.ir.ws.rest.converter;
- 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import com.zsmart.ir.service.util.*;
-import com.zsmart.ir.bean.IR; 
-import com.zsmart.ir.ws.rest.vo.IRVo; 
+import com.zsmart.ir.bean.IR;
+import com.zsmart.ir.ws.rest.vo.IRVo;
 
- @Component 
-public class IRConverter extends AbstractConverter<IR,IRVo>{ 
+@Component
+public class IRConverter extends AbstractConverter<IR, IRVo> {
 
-private boolean etatIR; 
+    private boolean etatIR;
 
- @Autowired
- private EtatIRConverter etatIRConverter ; 
-private boolean irEmployes; 
+    @Autowired
+    private EtatIRConverter etatIRConverter;
+    private boolean irEmployes;
 
- @Autowired
- private IREmployeConverter iREmployeConverter ; 
+    @Autowired
+    private IREmployeConverter iREmployeConverter;
 
- @Override 
- public IR toItem(IRVo vo) {
- if (vo == null) {
-    return null;
-      } else {
-IR item = new IR();
+    @Override
+    public IR toItem(IRVo vo) {
+        if (vo == null) {
+            return null;
+        } else {
+            IR item = new IR();
 
- if(etatIR&& vo.getEtatIRVo() != null) {
- item.setEtatIR(etatIRConverter.toItem(vo.getEtatIRVo()));
-} 
- 
- if (StringUtil.isNotEmpty(vo.getDescription())) {
- item.setDescription(vo.getDescription());
-} 
+            if (etatIR && vo.getEtatIRVo() != null) {
+                item.setEtatIR(etatIRConverter.toItem(vo.getEtatIRVo()));
+            }
 
- if (StringUtil.isNotEmpty(vo.getReference())) {
- item.setReference(vo.getReference());
-} 
+            if (StringUtil.isNotEmpty(vo.getDescription())) {
+                item.setDescription(vo.getDescription());
+            }
 
- if (StringUtil.isNotEmpty(vo.getReferenceSociete())) {
- item.setReferenceSociete(vo.getReferenceSociete());
-} 
+            if (StringUtil.isNotEmpty(vo.getReference())) {
+                item.setReference(vo.getReference());
+            }
 
- if (StringUtil.isNotEmpty(vo.getReferenceComptableValidateur())) {
- item.setReferenceComptableValidateur(vo.getReferenceComptableValidateur());
-} 
+            if (StringUtil.isNotEmpty(vo.getReferenceSociete())) {
+                item.setReferenceSociete(vo.getReferenceSociete());
+            }
 
- if (StringUtil.isNotEmpty(vo.getReferenceComptableCreateur())) {
- item.setReferenceComptableCreateur(vo.getReferenceComptableCreateur());
-} 
+            if (StringUtil.isNotEmpty(vo.getReferenceComptableValidateur())) {
+                item.setReferenceComptableValidateur(vo.getReferenceComptableValidateur());
+            }
 
- if (vo.getId() != null) {
- item.setId(NumberUtil.toLong(vo.getId()));
-} 
+            if (StringUtil.isNotEmpty(vo.getReferenceComptableCreateur())) {
+                item.setReferenceComptableCreateur(vo.getReferenceComptableCreateur());
+            }
 
- if (vo.getNombreEmployeeDeclaree() != null) {
- item.setNombreEmployeeDeclaree(NumberUtil.toInt(vo.getNombreEmployeeDeclaree()));
-} 
+            if (vo.getId() != null) {
+                item.setId(NumberUtil.toLong(vo.getId()));
+            }
 
- if (vo.getTotalSalaire() != null) {
- item.setTotalSalaire(NumberUtil.toBigDecimal(vo.getTotalSalaire()));
-} 
+            if (vo.getNombreEmployeeDeclaree() != null) {
+                item.setNombreEmployeeDeclaree(NumberUtil.toInt(vo.getNombreEmployeeDeclaree()));
+            }
 
- if (vo.getTotalPrime() != null) {
- item.setTotalPrime(NumberUtil.toBigDecimal(vo.getTotalPrime()));
-} 
+            if (vo.getTotalSalaire() != null) {
+                item.setTotalSalaire(NumberUtil.toBigDecimal(vo.getTotalSalaire()));
+            }
 
- if (vo.getMontantTotal() != null) {
- item.setMontantTotal(NumberUtil.toBigDecimal(vo.getMontantTotal()));
-} 
+            if (vo.getTotalPrime() != null) {
+                item.setTotalPrime(NumberUtil.toBigDecimal(vo.getTotalPrime()));
+            }
 
- if (vo.getMontantBaseIr() != null) {
- item.setMontantBaseIr(NumberUtil.toBigDecimal(vo.getMontantBaseIr()));
-} 
+            if (vo.getMontantTotal() != null) {
+                item.setMontantTotal(NumberUtil.toBigDecimal(vo.getMontantTotal()));
+            }
 
- if (vo.getMontantRetard() != null) {
- item.setMontantRetard(NumberUtil.toBigDecimal(vo.getMontantRetard()));
-} 
+            if (vo.getMontantBaseIr() != null) {
+                item.setMontantBaseIr(NumberUtil.toBigDecimal(vo.getMontantBaseIr()));
+            }
 
- if (vo.getMontantMajoration() != null) {
- item.setMontantMajoration(NumberUtil.toBigDecimal(vo.getMontantMajoration()));
-} 
+            if (vo.getMontantRetard() != null) {
+                item.setMontantRetard(NumberUtil.toBigDecimal(vo.getMontantRetard()));
+            }
 
- if (vo.getMontantPenalite() != null) {
- item.setMontantPenalite(NumberUtil.toBigDecimal(vo.getMontantPenalite()));
-} 
+            if (vo.getMontantMajoration() != null) {
+                item.setMontantMajoration(NumberUtil.toBigDecimal(vo.getMontantMajoration()));
+            }
 
- if (vo.getNomberMoisRetard() != null) {
- item.setNomberMoisRetard((vo.getNomberMoisRetard()));
-} 
+            if (vo.getMontantPenalite() != null) {
+                item.setMontantPenalite(NumberUtil.toBigDecimal(vo.getMontantPenalite()));
+            }
 
- if (vo.getAnne() != null) {
- item.setAnne((vo.getAnne()));
-} 
+            if (vo.getNomberMoisRetard() != null) {
+                item.setNomberMoisRetard(NumberUtil.toInt(vo.getNomberMoisRetard()));
+            }
 
- if (vo.getMois() != null) {
- item.setMois((vo.getMois()));
-} 
+            if (vo.getAnne() != null) {
+                item.setAnne(NumberUtil.toInt(vo.getAnne()));
+            }
 
- if (vo.getHonoraire() != null) {
- item.setHonoraire(NumberUtil.toBigDecimal(vo.getHonoraire()));
-} 
+            if (vo.getMois() != null) {
+                item.setMois(NumberUtil.toInt(vo.getMois()));
+            }
 
- if (vo.getPenalite() != null) {
- item.setPenalite(NumberUtil.toBigDecimal(vo.getPenalite()));
-} 
+            if (vo.getHonoraire() != null) {
+                item.setHonoraire(NumberUtil.toBigDecimal(vo.getHonoraire()));
+            }
 
- if (vo.getMajoration() != null) {
- item.setMajoration(NumberUtil.toBigDecimal(vo.getMajoration()));
-} 
+            if (vo.getPenalite() != null) {
+                item.setPenalite(NumberUtil.toBigDecimal(vo.getPenalite()));
+            }
 
- if (vo.getDateValidation() != null) {
- item.setDateValidation(DateUtil.parse(vo.getDateValidation()));
-} 
+            if (vo.getMajoration() != null) {
+                item.setMajoration(NumberUtil.toBigDecimal(vo.getMajoration()));
+            }
 
- if (vo.getDateSoumission() != null) {
- item.setDateSoumission(DateUtil.parse(vo.getDateSoumission()));
-} 
+            if (vo.getDateValidation() != null) {
+                item.setDateValidation(DateUtil.parse(vo.getDateValidation()));
+            }
 
- if (vo.getDateAffectationComptable() != null) {
- item.setDateAffectationComptable(DateUtil.parse(vo.getDateAffectationComptable()));
-} 
+            if (vo.getDateSoumission() != null) {
+                item.setDateSoumission(DateUtil.parse(vo.getDateSoumission()));
+            }
 
- if (vo.getDateFinalisation() != null) {
- item.setDateFinalisation(DateUtil.parse(vo.getDateFinalisation()));
-} 
+            if (vo.getDateAffectationComptable() != null) {
+                item.setDateAffectationComptable(DateUtil.parse(vo.getDateAffectationComptable()));
+            }
 
- if (vo.getDateSaisie() != null) {
- item.setDateSaisie(DateUtil.parse(vo.getDateSaisie()));
-} 
+            if (vo.getDateFinalisation() != null) {
+                item.setDateFinalisation(DateUtil.parse(vo.getDateFinalisation()));
+            }
 
- if (ListUtil.isNotEmpty(vo.getIREmployesVo()) && irEmployes) {
- item.setIrEmployes(iREmployeConverter.toItem(vo.getIREmployesVo())); 
-} 
+            if (vo.getDateSaisie() != null) {
+                item.setDateSaisie(DateUtil.parse(vo.getDateSaisie()));
+            }
 
-return item;
- }
- }
+            if (ListUtil.isNotEmpty(vo.getIREmployesVo()) && irEmployes) {
+                item.setIrEmployes(iREmployeConverter.toItem(vo.getIREmployesVo()));
+            }
 
-  @Override 
- public IRVo toVo(IR item) {
- if (item == null) {
-    return null;
-      } else {
-IRVo vo = new IRVo();
+            return item;
+        }
+    }
 
- if(etatIR&& item.getEtatIR() != null) {
- vo.setEtatIRVo(etatIRConverter.toVo(item.getEtatIR()));
-} 
- 
- if (StringUtil.isNotEmpty(item.getDescription())) {
- vo.setDescription(item.getDescription());
-} 
+    @Override
+    public IRVo toVo(IR item) {
+        if (item == null) {
+            return null;
+        } else {
+            IRVo vo = new IRVo();
 
- if (StringUtil.isNotEmpty(item.getReference())) {
- vo.setReference(item.getReference());
-} 
+            if (etatIR && item.getEtatIR() != null) {
+                vo.setEtatIRVo(etatIRConverter.toVo(item.getEtatIR()));
+            }
 
- if (StringUtil.isNotEmpty(item.getReferenceSociete())) {
- vo.setReferenceSociete(item.getReferenceSociete());
-} 
+            if (StringUtil.isNotEmpty(item.getDescription())) {
+                vo.setDescription(item.getDescription());
+            }
 
- if (StringUtil.isNotEmpty(item.getReferenceComptableValidateur())) {
- vo.setReferenceComptableValidateur(item.getReferenceComptableValidateur());
-} 
+            if (StringUtil.isNotEmpty(item.getReference())) {
+                vo.setReference(item.getReference());
+            }
 
- if (StringUtil.isNotEmpty(item.getReferenceComptableCreateur())) {
- vo.setReferenceComptableCreateur(item.getReferenceComptableCreateur());
-} 
+            if (StringUtil.isNotEmpty(item.getReferenceSociete())) {
+                vo.setReferenceSociete(item.getReferenceSociete());
+            }
 
- if (item.getId() != null) {
- vo.setId(NumberUtil.toString(item.getId()));
-} 
+            if (StringUtil.isNotEmpty(item.getReferenceComptableValidateur())) {
+                vo.setReferenceComptableValidateur(item.getReferenceComptableValidateur());
+            }
 
- if (item.getNombreEmployeeDeclaree() != null) {
- vo.setNombreEmployeeDeclaree(NumberUtil.toString(item.getNombreEmployeeDeclaree()));
-} 
+            if (StringUtil.isNotEmpty(item.getReferenceComptableCreateur())) {
+                vo.setReferenceComptableCreateur(item.getReferenceComptableCreateur());
+            }
 
- if (item.getTotalSalaire() != null) {
- vo.setTotalSalaire(NumberUtil.toString(item.getTotalSalaire()));
-} 
+            if (item.getId() != null) {
+                vo.setId(NumberUtil.toString(item.getId()));
+            }
 
- if (item.getTotalPrime() != null) {
- vo.setTotalPrime(NumberUtil.toString(item.getTotalPrime()));
-} 
+            if (item.getNombreEmployeeDeclaree() != null) {
+                vo.setNombreEmployeeDeclaree(NumberUtil.toString(item.getNombreEmployeeDeclaree()));
+            }
 
- if (item.getMontantTotal() != null) {
- vo.setMontantTotal(NumberUtil.toString(item.getMontantTotal()));
-} 
+            if (item.getTotalSalaire() != null) {
+                vo.setTotalSalaire(NumberUtil.toString(item.getTotalSalaire()));
+            }
 
- if (item.getMontantBaseIr() != null) {
- vo.setMontantBaseIr(NumberUtil.toString(item.getMontantBaseIr()));
-} 
+            if (item.getTotalPrime() != null) {
+                vo.setTotalPrime(NumberUtil.toString(item.getTotalPrime()));
+            }
 
- if (item.getMontantRetard() != null) {
- vo.setMontantRetard(NumberUtil.toString(item.getMontantRetard()));
-} 
+            if (item.getMontantTotal() != null) {
+                vo.setMontantTotal(NumberUtil.toString(item.getMontantTotal()));
+            }
 
- if (item.getMontantMajoration() != null) {
- vo.setMontantMajoration(NumberUtil.toString(item.getMontantMajoration()));
-} 
+            if (item.getMontantBaseIr() != null) {
+                vo.setMontantBaseIr(NumberUtil.toString(item.getMontantBaseIr()));
+            }
 
- if (item.getMontantPenalite() != null) {
- vo.setMontantPenalite(NumberUtil.toString(item.getMontantPenalite()));
-} 
+            if (item.getMontantRetard() != null) {
+                vo.setMontantRetard(NumberUtil.toString(item.getMontantRetard()));
+            }
 
- if (item.getNomberMoisRetard() != null) {
- vo.setNomberMoisRetard(NumberUtil.toString(item.getNomberMoisRetard()));
-} 
+            if (item.getMontantMajoration() != null) {
+                vo.setMontantMajoration(NumberUtil.toString(item.getMontantMajoration()));
+            }
 
- if (item.getAnne() != null) {
- vo.setAnne(NumberUtil.toString(item.getAnne()));
-} 
+            if (item.getMontantPenalite() != null) {
+                vo.setMontantPenalite(NumberUtil.toString(item.getMontantPenalite()));
+            }
 
- if (item.getMois() != null) {
- vo.setMois(NumberUtil.toString(item.getMois()));
-} 
+            if (item.getNomberMoisRetard() != null) {
+                vo.setNomberMoisRetard(NumberUtil.toString(item.getNomberMoisRetard()));
+            }
 
- if (item.getHonoraire() != null) {
- vo.setHonoraire(NumberUtil.toString(item.getHonoraire()));
-} 
+            if (item.getAnne() != null) {
+                vo.setAnne(NumberUtil.toString(item.getAnne()));
+            }
 
- if (item.getPenalite() != null) {
- vo.setPenalite(NumberUtil.toString(item.getPenalite()));
-} 
+            if (item.getMois() != null) {
+                vo.setMois(NumberUtil.toString(item.getMois()));
+            }
 
- if (item.getMajoration() != null) {
- vo.setMajoration(NumberUtil.toString(item.getMajoration()));
-} 
+            if (item.getHonoraire() != null) {
+                vo.setHonoraire(NumberUtil.toString(item.getHonoraire()));
+            }
 
- if (item.getDateValidation() != null) {
- vo.setDateValidation(DateUtil.formateDate(item.getDateValidation()));
-} 
+            if (item.getPenalite() != null) {
+                vo.setPenalite(NumberUtil.toString(item.getPenalite()));
+            }
 
- if (item.getDateSoumission() != null) {
- vo.setDateSoumission(DateUtil.formateDate(item.getDateSoumission()));
-} 
+            if (item.getMajoration() != null) {
+                vo.setMajoration(NumberUtil.toString(item.getMajoration()));
+            }
 
- if (item.getDateAffectationComptable() != null) {
- vo.setDateAffectationComptable(DateUtil.formateDate(item.getDateAffectationComptable()));
-} 
+            if (item.getDateValidation() != null) {
+                vo.setDateValidation(DateUtil.formateDate(item.getDateValidation()));
+            }
 
- if (item.getDateFinalisation() != null) {
- vo.setDateFinalisation(DateUtil.formateDate(item.getDateFinalisation()));
-} 
+            if (item.getDateSoumission() != null) {
+                vo.setDateSoumission(DateUtil.formateDate(item.getDateSoumission()));
+            }
 
- if (item.getDateSaisie() != null) {
- vo.setDateSaisie(DateUtil.formateDate(item.getDateSaisie()));
-} 
+            if (item.getDateAffectationComptable() != null) {
+                vo.setDateAffectationComptable(DateUtil.formateDate(item.getDateAffectationComptable()));
+            }
 
- if(ListUtil.isNotEmpty(item.getIrEmployes()) && irEmployes) {
- vo.setIREmployesVo(iREmployeConverter.toVo(item.getIrEmployes()));
-} 
+            if (item.getDateFinalisation() != null) {
+                vo.setDateFinalisation(DateUtil.formateDate(item.getDateFinalisation()));
+            }
 
-return vo;
- }
- }
-public void init() { 
+            if (item.getDateSaisie() != null) {
+                vo.setDateSaisie(DateUtil.formateDate(item.getDateSaisie()));
+            }
 
-etatIR = true; 
+            if (ListUtil.isNotEmpty(item.getIrEmployes()) && irEmployes) {
+                vo.setIREmployesVo(iREmployeConverter.toVo(item.getIrEmployes()));
+            }
 
-irEmployes = true; 
+            return vo;
+        }
+    }
+
+    public void init() {
+
+        etatIR = true;
+
+        irEmployes = true;
+    }
 }
- } 
